1eebc2570fb906093bbf71ce9f66b266c2ae6fbb
vaibhav garg <vaibhav.garg237@gmail.com>
2022-11-18 21:05:14 +0530

    deploy: server code update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const port = 5000;
+// const port = 5000;
+
+//FOR DEPLOYMENT ONLY
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const MONGODB_URI_final =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/netflix";
 
 const app = express();
+
+//Deployment testing
+app.get("/", (req, res) => {
+  console.log(process.env);
+  res.send("Testing my server");
+});
 
 const userRoutes = require("./routes/UserRoutes");
 
@@ -11,7 +23,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://localhost:27017/netflix", {
+  .connect(MONGODB_URI_final, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
